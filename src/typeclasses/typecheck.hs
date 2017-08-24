4077c24550aa26cd3f880373ec509f87daa8a2b3
Rene Vallenilla <rene.vallenilla@rea-group.com>
2017-08-24 12:06:40 +1000

    Add typecheck exercises

---
+++ src/typeclasses/typecheck.hs
@@ -0,0 +1,45 @@
+module Typecheck where
+
+data Person = Person Bool deriving Show
+
+printPerson :: Person -> IO ()
+printPerson person = putStrLn (show person)
+
+data Mood = Blah | Woot deriving (Show, Eq, Ord)
+
+-- instance Eq Mood where
+--   Woot == Woot = True
+
+settleDown x = if x == Woot then Blah else x
+
+type Subject = String
+type Verb = String
+type Object = String
+
+data Sentence =
+  Sentence Subject Verb Object
+  deriving (Eq, Show)
+
+s1 = Sentence "dogs" "drool" "them"
+s2 = Sentence "Julie" "loves" "dogs"
+
+data Rocks =
+  Rocks String deriving (Eq, Show, Ord)
+
+data Yeah =
+  Yeah Bool deriving (Eq, Show, Ord)
+
+data Papu =
+  Papu Rocks Yeah
+  deriving (Eq, Show, Ord)
+
+-- phew = Papu "chases" True # Not because it needs a Rocks not [Char]
+
+truth = Papu (Rocks "chomskydoz")
+             (Yeah True)
+
+equalityForall :: Papu -> Papu -> Bool
+equalityForall p p' = p == p'
+
+comparePapus :: Papu -> Papu -> Bool
+comparePapus p p' = p > p'
